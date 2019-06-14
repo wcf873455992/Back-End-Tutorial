9c4591eae9791ee5bb6f554cda3ee661378e6e7e
wcf873455992 <wcf873455992@sina.com>
2019-06-14 10:48:11 +0800

    swagger-ui ok

--- device-admin/src/main/java/com/example/device/admin/web/config/Swagger2.java
+++ device-admin/src/main/java/com/example/device/admin/web/config/Swagger2.java
@@ -53,4 +53,20 @@ public class Swagger2 {
                 .version("1.0")
                 .build();
     }
+
+    /**
+     * 防止@EnableMvc把默认的静态资源路径覆盖了，手动设置的方式
+     *
+     * @param registry
+     */
+//    @Override
+//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        // 解决静态资源无法访问
+//        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+//        // 解决swagger无法访问
+//        registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+//        // 解决swagger的js文件无法访问
+//        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+//
+//    }
 }
